28c3e7e5d54cfd5215139c306eedeec41aec56c0
John Mollberg <john@arccosgolf.com>
2022-12-02 00:04:56 -0600

    day 2 part 1

--- src/days/day-02.ts
+++ src/days/day-02.ts
@@ -1,21 +1,76 @@
 import {processInput} from '../utils/io-utils'
-import {initializeArray, sumArray} from '../utils/array-utils'
+
+type Move = 'rock' | 'paper' | 'scissors'
+type EnemyKey = 'A' | 'B' | 'C'
+type HeroKey = 'X' | 'Y' | 'Z'
+type Outcome = 'win' | 'draw' | 'loss'
+
+type MoveDetails = {
+    points: number
+    enemyKey: EnemyKey
+    heroKey: HeroKey
+    outcomes: Record<Move, Outcome>
+}
+
+const MOVE_DETAILS: Record<Move, MoveDetails> = {
+    rock: {
+        points: 1,
+        enemyKey: 'A',
+        heroKey: 'X',
+        outcomes: {
+            paper: 'loss',
+            scissors: 'win',
+            rock: 'draw',
+        },
+    },
+    paper: {
+        points: 2,
+        enemyKey: 'B',
+        heroKey: 'Y',
+        outcomes: {
+            paper: 'draw',
+            scissors: 'loss',
+            rock: 'win',
+        },
+    },
+    scissors: {
+        points: 3,
+        enemyKey: 'C',
+        heroKey: 'Z',
+        outcomes: {
+            paper: 'win',
+            scissors: 'draw',
+            rock: 'loss',
+        },
+    },
+} as const
+
+const BONUS_POINTS = {
+    loss: 0,
+    draw: 3,
+    win: 6,
+}
+
+const computeScore = (heroKey: HeroKey, enemyKey: EnemyKey): number => {
+    const moveDetails = Object.values(MOVE_DETAILS).find(details => details.heroKey === heroKey)
+    const enemyMove = Object.entries(MOVE_DETAILS).find(([_key, details]) => details.enemyKey === enemyKey)[0] as Move
+    return moveDetails.points + BONUS_POINTS[moveDetails.outcomes[enemyMove]]
+}
 
 export const runDay2 = async () => {
-    let someArray = initializeArray({
-        length: 3,
-        defaultValue: 0,
-    })
+    let totalScore = 0
+    const handleLine = line => {
+        const [enemyKey, heroKey]: [EnemyKey, HeroKey] = line.split(' ')
+        totalScore += computeScore(heroKey, enemyKey)
+    }
 
     await processInput({
         fileLocation: 'input.txt',
-        handleLine: (line) => {
-            console.log(`line: ${line}`)
-        }
+        handleLine,
     })
 
     console.log({
-        part1: Math.max(...someArray),
-        part2: sumArray(someArray),
+        part1: totalScore,
+        // part2: sumArray(someArray),
     })
 }
