28ec66862f946434bbd0f8044bb111e126c919c5
John Mollberg <john@arccosgolf.com>
2022-12-04 00:07:41 -0600

    day 4 part 2

--- src/days/day-04.ts
+++ src/days/day-04.ts
@@ -1,7 +1,6 @@
 import {processInput} from '../utils/io-utils'
-import {initializeArray, sumArray} from '../utils/array-utils'
 
-const computeRange = (assignment: string): [number, number] => {
+const extractElfAssignment = (assignment: string): [number, number] => {
     const [startStr, endStr] = assignment.split('-')
     return [
         parseInt(startStr),
@@ -9,37 +8,46 @@ const computeRange = (assignment: string): [number, number] => {
     ]
 }
 
-const getDoesFirstElfContainSecondElf = (firstElfAssignment: [number, number], secondElfAssignment: [number, number]) => {
+const getDoesFirstElfContainSecondElf = (firstElfAssignment: [number, number], secondElfAssignment: [number, number]): boolean => {
     const [firstElfStart, firstElfEnd] = firstElfAssignment
     const [secondElfStart, secondElfEnd] = secondElfAssignment
     return firstElfStart <= secondElfStart &&
         firstElfEnd >= secondElfEnd;
 }
 
-export const runDay4 = async () => {
-    let someArray = initializeArray({
-        length: 3,
-        defaultValue: 0,
-    })
+const getDoElvesOverlapAtAll = (firstElfAssignment: [number, number], secondElfAssignment: [number, number]): boolean => {
+    const [firstElfStart, firstElfEnd] = firstElfAssignment
+    const [secondElfStart, secondElfEnd] = secondElfAssignment
+    const maxStart = Math.max(firstElfStart, secondElfStart)
+    const minEnd = Math.min(firstElfEnd, secondElfEnd)
 
+    return minEnd >= maxStart
+}
+
+export const runDay4 = async () => {
     let numberOfCoveredAssignments = 0
+    let numberOfOverlappingAssignmentPairs = 0
     await processInput({
         fileLocation: 'input.txt',
         handleLine: (line) => {
             const [firstElf, secondElf] = line.split(',')
-            const firstElfAssignment = computeRange(firstElf)
-            const secondElfAssignment = computeRange(secondElf)
+            const firstElfAssignment = extractElfAssignment(firstElf)
+            const secondElfAssignment = extractElfAssignment(secondElf)
 
-            const doesFirstElfContainSecondElf = getDoesFirstElfContainSecondElf(firstElfAssignment, secondElfAssignment)
-            const doesSecondElfContainFirstElf = getDoesFirstElfContainSecondElf(secondElfAssignment, firstElfAssignment)
-            if (doesFirstElfContainSecondElf || doesSecondElfContainFirstElf) {
+            if (
+                getDoesFirstElfContainSecondElf(firstElfAssignment, secondElfAssignment) ||
+                getDoesFirstElfContainSecondElf(secondElfAssignment, firstElfAssignment)
+            ) {
                 numberOfCoveredAssignments += 1
+            }
+            if (getDoElvesOverlapAtAll(firstElfAssignment, secondElfAssignment)) {
+                numberOfOverlappingAssignmentPairs += 1
             }
         }
     })
 
     console.log({
         part1: numberOfCoveredAssignments,
-        part2: sumArray(someArray),
+        part2: numberOfOverlappingAssignmentPairs,
     })
 }
