5af24f7a2b7bfa76cbf893692600e62830734cec
John Mollberg <john@arccosgolf.com>
2022-12-09 12:47:10 -0600

    cleanup

--- src/days/day-09.ts
+++ src/days/day-09.ts
@@ -1,74 +1,82 @@
 import {getAllInputLines} from '../utils/io-utils'
-import {initializeArray, sumArray} from '../utils/array-utils'
-
-const getDeltaForTail = (headState: KnotState, [tailX, tailY]: [number, number]): [number, number] => {
-    const {
-        prevX,
-        prevY,
-        currentX,
-        currentY,
-    } = headState
-    if (
-        Math.abs(currentX - tailX) <= 1 &&
-        Math.abs(currentY - tailY) <= 1
-    ) {
-        return [0, 0]
-    }
-    if (tailX === currentX) {
-        return [0, currentY - prevY]
-    }
-    if (tailY === currentY) {
-        return [currentX - prevX, 0]
-    }
-    const deltaX = currentX > tailX ? 1 : -1
-    const deltaY = currentY > tailY ? 1 : -1
-    return [deltaX, deltaY]
-}
+import {initializeArray} from '../utils/array-utils'
 
 type Instruction = {
     direction: 'L' | 'R' | 'U' | 'D'
     distance: number
 }
 
-type KnotState = {
-    prevX: number
-    prevY: number
-    currentX: number
-    currentY: number
+type Knot = {
+    x: number
+    y: number
+}
+
+const getNewTailPosition = (
+    {
+        x: headX,
+        y: headY,
+    }: Knot,
+    {
+        x: tailX,
+        y: tailY,
+    }: Knot): Knot => {
+    const [
+        deltaX,
+        deltaY,
+    ] = (
+        Math.abs(headX - tailX) <= 1 &&
+        Math.abs(headY - tailY) <= 1
+    ) ?
+        [0, 0] :
+        [Math.sign(headX - tailX), Math.sign(headY - tailY)]
+    return {
+        x: tailX + deltaX,
+        y: tailY + deltaY,
+    }
 }
 
 const getNewHeadPosition = (
-    [currentX, currentY]: [number, number],
+    {x, y}: Knot,
     direction: Instruction['direction']
-): [number, number] => {
+): Knot => {
     switch (direction) {
         case 'L': {
-            return [currentX - 1, currentY]
+            return {
+                x: x - 1,
+                y,
+            }
         }
         case 'R': {
-            return [currentX + 1, currentY]
+            return {
+                x: x + 1,
+                y,
+            }
         }
         case 'U': {
-            return [currentX, currentY - 1]
+            return {
+                x,
+                y: y - 1,
+            }
         }
         case 'D': {
-            return [currentX, currentY + 1]
+            return {
+                x,
+                y: y + 1,
+            }
         }
         default: {
-            throw new Error('unexpected Direction')
+            throw new Error(`unexpected direction: ${direction}`)
         }
     }
 }
 
 const processInstructions = (instructions: Instruction[], ropeLength: number) => {
     const coordinatesVisited = new Set<string>()
-    const knots = initializeArray<KnotState>({
+    const knots = initializeArray<Knot>({
         length: ropeLength,
         defaultValue: {
-            prevX: 0,
-            prevY: 0,
-            currentX: 0,
-            currentY: 0,
+            x: 0,
+            y: 0,
         }
     })
     instructions.forEach(({
@@ -76,42 +84,29 @@ const processInstructions = (instructions: Instruction[], ropeLength: number) =>
                               distance,
                           }) => {
         for (let i = 0; i < distance; i++) {
-            const {
-                currentX,
-                currentY,
-            } = knots[0]
-            const [newX, newY] = getNewHeadPosition([currentX, currentY], direction)
-            knots[0].prevX = currentX
-            knots[0].prevY = currentY
-            knots[0].currentX = newX
-            knots[0].currentY = newY
-            knots.slice(1).forEach((knot, knotIndex) => {
-                const {
-                    currentX,
-                    currentY,
-                } = knot
-                const [deltaX, deltaY] = getDeltaForTail(
-                    knots[knotIndex],
-                    [currentX, currentY],
+            // move head
+            knots.fill(
+                getNewHeadPosition(knots[0],
+                    direction),
+                0,
+                1,
+            )
+            // move all tails
+            knots.slice(1).forEach((knot, knotIndex) =>
+                knots.fill(
+                    getNewTailPosition(knots[knotIndex], knot),
+                    knotIndex + 1,
+                    knotIndex + 2,
                 )
-                knot.prevX = currentX
-                knot.prevY = currentY
-                knot.currentX = currentX + deltaX
-                knot.currentY = currentY + deltaY
-            })
+            )
             const lastKnot = knots[knots.length - 1]
-            coordinatesVisited.add(`${lastKnot.currentX},${lastKnot.currentY}`)
+            coordinatesVisited.add(`${lastKnot.x},${lastKnot.y}`)
         }
     })
     return coordinatesVisited.size
 }
 
 export const runDay9 = async () => {
-    let someArray = initializeArray({
-        length: 3,
-        defaultValue: 0,
-    })
-
     const instructions: Instruction[] = getAllInputLines({fileLocation: 'input.txt'})
         .map(line => {
             const [direction, distanceStr] = line.split(' ')
