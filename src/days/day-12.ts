15492d04a41510b010bffc316d96cc721825b969
John Mollberg <john@arccosgolf.com>
2022-12-12 16:41:08 -0600

    day 12 part 1

--- src/days/day-12.ts
+++ src/days/day-12.ts
@@ -5,65 +5,85 @@ type Tile = {
     stepsToTarget: number
     isStartingPoint: boolean
     isTarget: boolean
+    rowIndex: number
+    colIndex: number
+    reachableNeighbors: Tile[]
 }
 
 export const runDay12 = async () => {
     const map: Tile[][] = getAllInputLines({
         fileLocation: 'input.txt',
-    }).map(line => line.split('').map(char => {
-        if (char === 'S') {
-            return {
-                elevation: 0,
+    }).map((line, rowIndex) => line
+        .split('')
+        .map((char, colIndex) => {
+            if (char === 'S') {
+                return {
+                    elevation: 0,
+                    stepsToTarget: Number.MAX_SAFE_INTEGER,
+                    isStartingPoint: true,
+                    isTarget: false,
+                    rowIndex,
+                    colIndex,
+                    isVisited: false,
+                    reachableNeighbors: [],
+                    isMarked: false,
+                }
+            }
+            if (char === 'E') {
+                return {
+                    elevation: 25,
+                    stepsToTarget: 0,
+                    isStartingPoint: false,
+                    isTarget: true,
+                    rowIndex,
+                    colIndex,
+                    isVisited: true,
+                    reachableNeighbors: [],
+                    isMarked: true,
+                }
+            }
+            return ({
+                elevation: char.charCodeAt(0) - 97,
                 stepsToTarget: Number.MAX_SAFE_INTEGER,
-                isStartingPoint: true,
-                isTarget: false,
-            }
-        }
-        if (char === 'E') {
-            return {
-                elevation: 25,
-                stepsToTarget: 0,
                 isStartingPoint: false,
-                isTarget: true,
-            }
-        }
-        return ({
-            elevation: char.charCodeAt(0) - 97,
-            stepsToTarget: Number.MAX_SAFE_INTEGER,
-            isStartingPoint: false,
-            isTarget: false,
-        });
-    }))
+                isTarget: false,
+                rowIndex,
+                colIndex,
+                isVisited: false,
+                reachableNeighbors: [],
+                isMarked: false,
+            });
+        }))
 
-    const startingTile = map.flat().find(tile => tile.isStartingPoint)
-
-    while (startingTile.stepsToTarget === Number.MAX_SAFE_INTEGER) {
-        for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
-            const row = map[rowIndex]
-            for (let colIndex = 0; colIndex < row.length; colIndex++) {
-                const currentTile = row[colIndex]
-                if (currentTile.stepsToTarget !== Number.MAX_SAFE_INTEGER) {
-                    continue
-                }
-                const neighboringTiles = [
-                    row[colIndex - 1],
-                    row[colIndex + 1],
-                    (map[rowIndex - 1] || [])[colIndex],
-                    (map[rowIndex + 1] || [])[colIndex],
-                ].filter(Boolean)
-                    .filter(tile => Math.abs(tile.elevation - currentTile.elevation) <= 1)
-                const min = Math.min(...neighboringTiles.map(tile => tile.stepsToTarget))
-                if (min < Number.MAX_SAFE_INTEGER) {
-                    currentTile.stepsToTarget = min + 1
-                }
-            }
-        }
+    const getNeighboringTiles = (tile: Tile): Tile[] => {
+        return [
+            map[tile.rowIndex][tile.colIndex - 1],
+            map[tile.rowIndex][tile.colIndex + 1],
+            (map[tile.rowIndex - 1] || [])[tile.colIndex],
+            (map[tile.rowIndex + 1] || [])[tile.colIndex],
+        ].filter(Boolean)
+            .filter(neighbor => [0, 1].includes(neighbor.elevation - tile.elevation) || tile.elevation > neighbor.elevation)
     }
 
+    map
+        .flat()
+        .forEach(tile => tile.reachableNeighbors = getNeighboringTiles(tile))
 
+    const startTile = map.flat().find(tile => tile.isStartingPoint)
+
+    while (startTile.stepsToTarget === Number.MAX_SAFE_INTEGER) {
+        map.flat().forEach(tile => {
+            const neighborSteps = tile.reachableNeighbors
+                .map(neighbor => neighbor.stepsToTarget)
+                .filter(distance => distance < Number.MAX_SAFE_INTEGER)
+            if (neighborSteps.length) {
+                tile.stepsToTarget = Math.min(...neighborSteps) + 1
+            }
+        })
+    }
 
     console.log({
-        part1: startingTile.stepsToTarget,
+        part1: startTile.stepsToTarget,
         // part2: sumArray(someArray),
     })
 }
